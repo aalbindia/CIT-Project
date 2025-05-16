7beb3e15d04e1dc6c3011b38040e44a976756af6
allen <aalbindia@my.bcit.ca>
2025-05-15 21:44:49 -0700

    Removing deleted

--- models/campus.py
+++
@@ -1,8 +0,0 @@
-from db import db
-
-class Campus(db.Model):
-    __tablename__ = "campuses"
-
-    id = db.mapped_column(db.Integer, primary_key=True)
-    name = db.mapped_column(db.String)
-    cars = db.relationship("Car", back_populates="campus")
